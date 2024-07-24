1ccd895eaad9ae025c29dc39802d5f669de7a5fc
KBHyeon <kim8731kim@naver.com>
2024-07-24 11:01:11 +0900

    LoPol 0723
    UniData Dictionary로 관리
    TitleScene 기본구성(씬 이동 등)

--- Assets/Project_UD/Scripts/UD_UnitDataManager.cs
+++ Assets/Project_UD/Scripts/UD_UnitDataManager.cs
@@ -2,25 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UD_UnitDataManager;
 
 public class UD_UnitDataManager : MonoBehaviour
 {
-    public enum UnitClass
-    {
-        Militiaman,
-        Spearman,
-        DolGyukByeong,
-        Archer,
-        Crossbowman,
-        Hunter,
-        Taoist,
-        Shamanist,
-        FengShui
-    }
-
-
     public class UnitData
     {
         public string Name;
@@ -37,85 +24,58 @@ public class UD_UnitDataManager : MonoBehaviour
             HP = hp;
             Material = material;
         }
+    }
+
+
+    private Dictionary<string, UnitData> unitDataDictionary = new Dictionary<string, UnitData>();
+
+
+    void Start()
+    {
+        ShowUnitData();
+
+        UnitData unit = GetUnitData("민병");
+        if (unit != null)
+        {
+            Debug.Log($"Fetched Unit Data -> Name: {unit.Name}, Class: {unit.Type}, Level: {unit.Level}, HP: {unit.HP}, Material: {unit.Material}");
+        }
+    }
+
+    public void SetUnitData(List<UnitData> unitDataList)
+    {
+        foreach(var  unitData in unitDataList)
+        {
+            unitDataDictionary[unitData.Name] = unitData;
+            Debug.Log($"UnitName: {name}, Class: {unitData.Type}, Level: {unitData.Level}, HP: {unitData.HP}, Material: {unitData.Material}");
+        }
 
         
     }
 
-    private List<UnitData> unitDatas = new List<UnitData>();
 
-    private Dictionary<UnitClass, UnitData> unitDataDictionary = new Dictionary<UnitClass, UnitData>();
-
-    public void SetUnitData(List<UnitData> unitData)
+    public UnitData GetUnitData(string unitType)
     {
-        unitDatas = unitData;
-        ShowUnitData();
-    }
-
-
-
-    //public void SetUnitData(UnitClass unitClass, UnitData unitData)
-    //{
-    //    unitDataDictionary[unitClass] = unitData;
-    //    Debug.Log($"Set data for {unitClass}: Name={unitData.Name}, Level={unitData.Level}, HP={unitData.HP}, Material={unitData.Material}");
-    //}
-
-    public UnitData GetUnitData(UnitClass unitClass)
-    {
-        if (unitDataDictionary.TryGetValue(unitClass, out UnitData unitData))
+        if(unitDataDictionary.TryGetValue(unitType, out UnitData unitData))
         {
             return unitData;
         }
         else
         {
+            Debug.Log("데이터 없음");
             return null;
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
-
     
-    void ShowUnitData()
+    public void ShowUnitData()
     {
-        if (unitDatas.Count == 0)
+        foreach(var unitdata in unitDataDictionary)
         {
-            return;
+            var unitType = unitdata.Key;
+            var unitData = unitdata.Value;
+            Debug.Log($"UnitName: {name}, Class: {unitData.Type}, Level: {unitData.Level}, HP: {unitData.HP}, Material: {unitData.Material}");
         }
 
-        foreach (var unit in unitDatas)
-        {
-            Debug.Log($"Name: {unit.Name}, Type: {unit.Type}, Level: {unit.Level}, HP: {unit.HP}, Material: {unit.Material}");
-        }
-    }
-
-    public UnitClass ParseUnitClass(string unitClass)
-    {
-        switch (unitClass)
-        {
-            case "민병":
-                return UnitClass.Militiaman;
-            case "창병":
-                return UnitClass.Spearman;
-            case "돌격병":
-                return UnitClass.DolGyukByeong;
-            case "궁수":
-                return UnitClass.Archer;
-            case "노병":
-                return UnitClass.Crossbowman;
-            case "사냥꾼":
-                return UnitClass.Hunter;
-            case "도사":
-                return UnitClass.Taoist;
-            case "주술도사":
-                return UnitClass.Shamanist;
-            case "풍수도사":
-                return UnitClass.FengShui;
-            default:
-                return UnitClass.Militiaman;
-        }
     }
 
 }
