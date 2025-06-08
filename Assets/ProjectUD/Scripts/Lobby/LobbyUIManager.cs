24aaf6553acd66315398e5b39c34a98b5cea810d
KBHyeon <kim8731kim@naver.com>
2025-06-08 21:33:02 +0900

    0608 LoPol
    머지 후 작업 정리

--- Assets/ProjectUD/Scripts/Lobby/LobbyUIManager.cs
+++ Assets/ProjectUD/Scripts/Lobby/LobbyUIManager.cs
@@ -182,7 +182,7 @@ public class LobbyUIManager : MonoBehaviour
             {
                 SoundManager.Instance.PlaySFX(battleStartSfx);
 
-                LoadingSceneManager.LoadScene("Stage1_MergeScene  25.0602");
+                LoadingSceneManager.LoadScene("Stage1_MergeScene  25.0608");
 
                 if (GlobalSoundManager.instance != null)
                 {
