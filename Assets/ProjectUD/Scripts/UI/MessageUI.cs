459f4757488f79790f2942d0c6572e88cc8841db
dkdyzhd <133553839+dkdyzhd@users.noreply.github.com>
2025-10-20 15:55:47 +0900

    MessageUI(Text) 시스템
    - 작업 완료
    - 프리팹화 완료
    - 경복궁 > 남한산성 이름 변경

--- Assets/ProjectUD/Scripts/UI/MessageUI.cs
+++ Assets/ProjectUD/Scripts/UI/MessageUI.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using TMPro;
 using UnityEngine;
 
@@ -7,21 +8,61 @@ public class MessageUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI message;
     [SerializeField] private AudioClip sfx;
+    [SerializeField] private float duration;
+    private float showTimer;
     private Animator animator;
-    private List<string> messages = new List<string>();
+    public List<string> messages = new List<string>();
+
 
     private void Start()
     {
         animator = GetComponent<Animator>();
     }
 
-    // Start is called before the first frame update
-    public void Show(string str)
+    private void Update()
     {
+        if(showTimer > 0f)
+        {
+            showTimer -= Time.deltaTime;
+
+            if (showTimer <= 0f)
+            {
+                animator.SetTrigger("FadeOut"); // 끄는 애니메이션 호출 Fadeout
+                RemoveMessage();
+            }
+        }
+    }
+
+    public void AddMessage(string message)  // 버튼 클릭 메서드용
+    {
+        if (!messages.Contains(message))
+        {
+            messages.Add(message);
+
+            if (messages.Count == 1)
+            {
+                ShowNextMessage();
+            }
+        }
+    }
+    private void ShowNextMessage()
+    {
+        showTimer = duration;
+        string str = messages[0];
         message.text = str;
         message.gameObject.SetActive(true);
-        animator.SetTrigger("Show");
+        animator.SetTrigger("FadeIn");
         SoundManager.Instance.PlaySFX(sfx);
 
+        //PlayerPrefs.GetInt("IsFirstStageClear");
+    }
+
+    private void RemoveMessage()
+    {
+        messages.RemoveAt(0);
+        if(messages.Count > 0)
+        {
+            ShowNextMessage();
+        }
     }
 }
